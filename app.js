1aa0aa79caf5999349e36724fd62000619d8ef3f
marek2222 <marekmusial2@o2.pl>
2018-11-14 15:11:38 +0100

    nodeSeriate: 01_3:  Install by bower manager: bootstrap and  jquery. Ada hamburger menu to layout.pug.

--- app.js
+++ app.js
@@ -14,11 +14,14 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// Set Public folder: to images, Css, files
+app.use(express.static(path.join(__dirname, 'public')));
+  
 // Body Parser Middleware - To handle HTTP POST request in Express.js version 4 and above, you need to install middleware module called body-parser. body-parser extract the entire body portion of an incoming request stream and exposes it on req.body.
-// parse application/json
-app.use(bodyParser.json());
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
 
 //Home route
 app.get('/', function(req, res){
@@ -32,51 +35,41 @@ app.get('/', function(req, res){
     }, function (err){
         console.log ("Something bad happened:", err);
     });
-
-
-
-    // sql.getPlainContext()
-	// .step( "readUsers", {
-    //     query: sql.fromFile('./sql/baza')
-	// 	// optionally you could do this if the
-	// 	// above query were in a readUsers.sql file
-	// 	// query: sql.fromFile( "readUsers" );
-	// })
-	// .end( function( sets ){
-	// 	// sets has a "readUsers" property
-    //     // which contains the results of the query
-    //     // console.log(sets[0]);
-        
-    //     res.render('index', {
-    //         tytul:'Artykuły', 
-    //         artykuly: sets
-    //     });
-    // })
-	// .error( function( err ){
-	// 	console.log( err );
-    // });
-    
-    // sql.execute({
-    //     query: sql.fromFile('./sql/baza')
-    // }).then( function(results){
-    //     agents = results;
-    // }, function (err){
-    //     console.log ('Something bad happened:', err);
-    // });
-    // console.log(agents[0])
-    // res.render('index', {
-    //     title2:'title', 
-    //     agents: agents
-    // });
 });
 
 // Add route
-app.get('/articles/add', function(req, res){
-    res.render('add_article', {
-        title: 'Add Article'
+app.get('/artykuly/dodaj', function(req, res){
+    res.render('dodaj_artykul', {
+        tytul: 'Dodaj Artykuł'
     });
 });
- 
+
+// Add Submit POST Route
+app.post('/artykuly/dodaj', function(req, res){
+    sql.execute({
+        query: sql.fromFile('./sql/dodaj_artykul'),        // query: 'insert nodejs.dbo.[_b_Articles](tytul,autor,cialo) select tytul,autor,cialo from @tabela',
+        params: {
+            tabela: {
+                val: [{   tytul: req.body.tytul, 
+                            autor: req.body.autor, 
+                            cialo: req.body.cialo }],
+                asTable: {
+                    tytul:     sql.NVARCHAR(50),
+                    autor:    sql.NVARCHAR(50),
+                    cialo:     sql.NVARCHAR(50)
+                }
+            }
+        }
+    }).then( function(results){
+        res.redirect('/');
+    }, function (err){
+        console.log ('Something bad happened:', err);
+    });
+});
+    
+    
+    
+  
 /// Start server
 app.listen(port, function(){
     console.log('Example app listening on port 3000!');
---
+++ sql/dodaj_artykul.sql
@@ -0,0 +1,3 @@
+insert nodejs.dbo.Artykuly 
+            (tytul,autor,cialo) 
+select   tytul,autor,cialo from @tabela
