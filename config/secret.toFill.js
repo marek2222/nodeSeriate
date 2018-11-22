a7073e08067ec9260a2e4aaa6146ad49bdcd9e10
marek2222 <marekmusial2@o2.pl>
2018-11-22 21:19:05 +0100

    nodeSeriate: 02_1:  Add config/secret.js to .gitignore

---
+++ config/secret.toFill.js
@@ -0,0 +1,10 @@
+// You schould be change the name to secret.js. Next  
+
+const config = {
+    name: 'default',
+    server: 'serverInstance',              // server:  'localhost\\SQLEXPRESS',
+    user: 'user',                                 // user: 'sa',
+    password: 'password',
+    database: 'database'
+}
+module.exports = config;
